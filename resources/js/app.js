cac86a73012c7571630fd5300f13857d93349d0e
bug41 <bugkinguswit@gmail.com>
2022-10-15 12:20:46 +0900

    vuex 버전 3.4.0 으로 변경

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,7 @@ require('./bootstrap');
 
 import router from "./routes";
 import VueRouter from "vue-router";
+import Vuex from 'vuex';
 import Index from "./Index";
 import moment from "moment";
 import StarRating from "./shared/components/StarRating.vue";
@@ -16,6 +17,8 @@ window.Vue = require('vue');
 // Vue.component('example-2', require('./components/Example2.vue').default);
 
 Vue.use(VueRouter);
+Vue.use(Vuex);
+
 Vue.filter("fromNow", value => moment(value).fromNow());
 
 Vue.component("star-rating", StarRating);
@@ -23,9 +26,22 @@ Vue.component("fatal-error", FatalError);
 Vue.component("success", Success);
 Vue.component("v-errors", ValidationErrors);
 
+
+const store = new Vuex.Store({
+    state : {
+        count: 0
+    },
+    mutations: {
+        increment(state){
+            state.count++
+        }
+    }
+});
+
 const app = new Vue({
     el: '#app',
     router,
+    store,
     components: {
         "index": Index
     }
