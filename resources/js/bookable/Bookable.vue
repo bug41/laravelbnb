c42fc14f93c6594a92d8e977cc0f6207736d229a
bug41 <bugkinguswit@gmail.com>
2022-10-25 21:18:37 +0900

    store.js basket 추가

--- resources/js/bookable/Bookable.vue
+++ resources/js/bookable/Bookable.vue
@@ -21,7 +21,7 @@
         <price-breakdown v-if="price" :price="price" class="mb-4"></price-breakdown>        
       </transition>
       <transition name="fade">        
-        <button class="btn btn-outline-secondary btn-block" v-if="price">Book now</button>
+        <button class="btn btn-outline-secondary btn-block" v-if="price" @click="addToBasket">Book now</button>
       </transition>
 
 
@@ -74,6 +74,13 @@ export default {
       } catch (err) {
         this.price = null;
       }
+    },
+    addToBasket() {
+      this.$store.commit("addToBasket", {
+        bookable : this.bookable,
+        price : this.price,
+        dates: this.lastSearch
+      });
     }
   }
 };
